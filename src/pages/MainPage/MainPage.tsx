6007b88e3892ce8ce111e805931c62e1b8533da9
Olimpieva Natalya <ukurennyi.koshak@gmail.com>
2024-04-09 00:32:09 +0300

    feat: add page skeleton

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,65 @@
 import React from "react";
 
-const MainPage = () => <div>It's gonna be something</div>;
+const MainPage = () => (
+  <div>
+    <h1>Наталья Олимпиева</h1>
+    <h3>Фронтенд-разработчик</h3>
+
+    <p>Люблю и умею искать простые решения сложных задач.</p>
+
+    <div>
+      <h2>Контакты</h2>
+      <ul>
+        <li>Telegram</li>
+        <li>Email</li>
+        <li>Github</li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>Работа</h2>
+      <ul>
+        <li>SMM Games</li>
+        <li>Jiff food</li>
+        <li>Фриланс</li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>Основные навыки</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>SASS</li>
+        <li>Javascript</li>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Typescript</li>
+        <li>Next.js</li>
+        <li>Git</li>
+        <li>Webpack</li>
+        <li>Docker</li>
+        <li>Playwright</li>
+        <li>Jest</li>
+        <li>Three.js</li>
+      </ul>
+    </div>
+
+    <div>
+      <h2>Образование</h2>
+      <ul>
+        <li>
+          Курский государственный университет - математическое моделирование
+        </li>
+        <li>
+          Курский государственный университет - информационная безопасность
+        </li>
+        <li>Яндекс - веб-разработчик</li>
+        <li>Javascript.ru - React + Redux</li>
+        <li>Udemy - Typescript</li>
+      </ul>
+    </div>
+  </div>
+);
 
 export default MainPage;
