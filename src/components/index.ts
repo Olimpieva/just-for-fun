88006b2d30c3af24baa050ee05761e12a333140c
Olimpieva Natalya <ukurennyi-koshak@yandex.ru>
2024-04-11 12:44:31 +0300

    style: main page && refactor: fonts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import Button from "./Button";
 import Link from "./Link";
 import Card from "./Card";
+import RainbowTitle from "./RainbowTitle";
+import TypingText from "./TypingText";
 
-export { Button, Link, Card };
+export { Button, Link, Card, TypingText, RainbowTitle };
